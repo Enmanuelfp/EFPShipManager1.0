1cd7b1ec308d652ee15d76c4b0a30cdcc12ff8fe
Enmanuel <emmanuelperez11@gmail.com>
2023-07-28 21:02:51 -0400

    Resolucion de errores

--- controlEnvios/src/main/java/cl/efpg/controlEnvios/entity/Usuario.java
+++ controlEnvios/src/main/java/cl/efpg/controlEnvios/entity/Usuario.java
@@ -1,6 +1,5 @@
 package cl.efpg.controlEnvios.entity;
 
-import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -11,7 +10,6 @@ public class Usuario {
 	
 	@Id
 	protected int id;
-	@Basic
     protected String nombre;
     protected String apellido;
     protected String run;
@@ -20,5 +18,118 @@ public class Usuario {
     protected String correoElectronico;
     protected int numeroTelefononico;
     protected String tipoUsuario;
+    
+    
+    public Usuario () {
+    	
+    }
+    
+    
+	public Usuario(int id, String nombre, String apellido, String run, String edad, String direccion,
+			String correoElectronico, int numeroTelefononico, String tipoUsuario) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.run = run;
+		this.edad = edad;
+		this.direccion = direccion;
+		this.correoElectronico = correoElectronico;
+		this.numeroTelefononico = numeroTelefononico;
+		this.tipoUsuario = tipoUsuario;
+	}
 
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+
+	public String getRun() {
+		return run;
+	}
+
+
+	public void setRun(String run) {
+		this.run = run;
+	}
+
+
+	public String getEdad() {
+		return edad;
+	}
+
+
+	public void setEdad(String edad) {
+		this.edad = edad;
+	}
+
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+	public String getCorreoElectronico() {
+		return correoElectronico;
+	}
+
+
+	public void setCorreoElectronico(String correoElectronico) {
+		this.correoElectronico = correoElectronico;
+	}
+
+
+	public int getNumeroTelefononico() {
+		return numeroTelefononico;
+	}
+
+
+	public void setNumeroTelefononico(int numeroTelefononico) {
+		this.numeroTelefononico = numeroTelefononico;
+	}
+
+
+	public String getTipoUsuario() {
+		return tipoUsuario;
+	}
+
+
+	public void setTipoUsuario(String tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
+	
+	
+    
+    
 }
--- controlEnvios/src/main/java/cl/efpg/controlEnvios/repository/IUsuarioRepository.java
+++ controlEnvios/src/main/java/cl/efpg/controlEnvios/repository/IUsuarioRepository.java
@@ -1,9 +1,10 @@
 package cl.efpg.controlEnvios.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import cl.efpg.controlEnvios.entity.Usuario;
-
+@Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
 
 }
